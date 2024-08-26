90a18c1ce51615f6dc011e6fa2c5ef587183d348
charanpreet4 <charanpreet@indee.tv>
2024-08-26 14:11:48 +0530

    removed src

--- src/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-/**
- * Always keep pure function inside utit folder
- * Any function having side effects should be kept outside of util folder
- * For eg in context
- **/
-
-const formatDate = () =>{
-    return ''
-}
