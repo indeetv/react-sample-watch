711c5aa7439f64fadfb72600244308d4f735d6f8
charanpreet4 <charanpreet@indee.tv>
2024-08-26 14:15:00 +0530

    added files inside src

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <span className='text-3xl font-bold underline'>Sample Watch React App</span>
+    </>
+  )
+}
+
+export default App
